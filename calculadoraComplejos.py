41fafa4a223ac651d6b6c17e39d577ac71de498d
unknown <juan.herrera-m@labinfo.is.escuelaing.edu.co>
2020-01-21 15:34:33 -0500

    Añado algunas Funciones LAB2

--- calculadoraComplejos.py
+++ calculadoraComplejos.py
@@ -1,7 +1,18 @@
 import math 
 
-num1 = [1,(3)**0.5]
-num2 = [1,0]
+num1 = [6,-4,7,3,4.2,-8.1,0,-3]
+num2 = [16,2.3,0,-7,6,0,0,-4]
+
+
+vector1 =[[6, -4], [7, 3], [4.2, -8.1], [0, -3]]
+vector2 =[[16, 2.3], [0, -7], [6, 0], [0, -4]]
+vectorFinal = []
+
+
+matriz1 = [[[1,1],[2,-3]] , [[4,2],[2,4]]]
+matriz2 = [[[0,0],[0,0]] , [[0,0],[0,0]]]
+matrizFinal = []
+
 c = [0,0]
 def sumar(num1,num2):
     resuE = (num1[0])+(num2[0])
@@ -41,7 +52,9 @@ def modulo(num1):
     resu = ((num1[0]**2)+(num1[1]**2))**0.5
     c[0],c[1]=resu,"null"
     return resu
-
+def inversa(num1):
+    c[0],c[1]= num1[0]*-1,num1[1]*-1
+    return  c[0],c[1]
 def conjugado(num1):
     c[0],c[1]=num1[0],(num1[1]*-1)
     return (c[0],c[1])
@@ -58,7 +71,29 @@ def cartesianasPolares(num1):
         angulo = 180- angulo
     c[0],c[1]= hipo ,angulo
     return (hipo,angulo)
-    
+
+
+def sumaVectores(vec1,vec2):
+    if len(vec1)== len(vec2):
+        for i in range(len(vec1)):
+            vectorFinal.append(sumar(vec1[i],vec2[i]))
+    return vectorFinal
+
+
+
+def inversaVectores(vec1):
+    for i in range(len(vec1)):
+        vectorFinal.append(inversa(vec1[i]))
+    return vectorFinal
+
+
+def adicionMat(mat1,mat2):
+    for i in range(len(mat1)):
+        aux = []
+        for j in range(len(mat1[0])):
+            
+
+
 def prettyPrinting(c):
     if c[1]!="null":
         if c[1]>=0:
@@ -68,15 +103,33 @@ def prettyPrinting(c):
     else:
         print(str(c[0]))
 
+
+def prettyprintingVectores(vf):
+    vectorPintar = []
+    if vectorFinal == [""]:
+        print("No se pudo Sumar vector , Longitudes diferentes")
+    else:
+        for i in vf:
+            if (i[1]>0):
+                print(str(i[0])+"+"+str(i[1])+"i")
+            else:
+                print(str(i[0])+str(i[1])+"i")
+    
+    
+
+
         
+
 def main():
-    cartesianasPolares(num1)
+##    inversaVectores(vector1)
+##    sumaVectores(vector1,vector2)
+##    cartesianasPolares(num1)
 ##    multiplicar(num1,num2)
 ##    sumar(num1,num2)
 ##    restar(num1,num2)
 ##    divicion(num1,num2)
 ##    modulo(num1)
 ##    conjugado(num1)
-    prettyPrinting(c)
-    
+##    prettyPrinting(c)
+##    prettyprintingVectores(vectorFinal)
 main()
